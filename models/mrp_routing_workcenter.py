7d13ec7ad07f94f2fd046b1bc0a326be32640090
viernes <luis.muro@gmail.com>
2018-06-28 11:30:55 -0500

    cambie las carpetas

--- models/mrp_routing_workcenter.py
+++
@@ -1,5 +0,0 @@
-from odoo import models, fields, api
-
-class MrpRoutingWorkorder(models.Model):
-    _inherit = 'mrp.routing.workcenter'
-    payment = fields.Float(string="Pago de empleado por pza.")
